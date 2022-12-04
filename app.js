0da1e17d9a4693a749d420b2f5afa115ad44d603
MichaelR-Dev <kmedrang@gmail.com>
2022-12-04 18:39:41 -0500

    Update Members

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const team = ["-","Brent","Carlos","Chris","Devin","Eric","Jesse","Joey","Jovanny","Keyshawn","Lily","Marco","Marcus","MJ","MR","Ty"]
+const team = ["-","Brent","Carlos","Chris","Devin","Eric", "ET", "Jesse","Joey","Jovanny","Keyshawn","Lily","Marco","Marcus","MJ","MR","Ty"]
+const selected = []
 
 let workSelectors = document.getElementsByClassName("workers");
 let dateSelector = document.getElementById("date");
@@ -12,10 +13,17 @@ dateSelector.setAttribute('value', today);
 for (const element of workSelectors) {
     team.forEach(member => {
         console.log(member);
+
         let option = new Option(member, member);
+        option.onclick = selectMember;
+        
         element.appendChild(option)
     }); 
 }
 
+const selectMember = () => {
 
-    
+    for (const element of workSelectors) {
+        if(element)
+    }
+}
