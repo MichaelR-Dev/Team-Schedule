454d0256058a5dc7c012f38b77ddea46c659bac8
MichaelR-Dev <kmedrang@gmail.com>
2022-12-06 11:27:54 -0500

    Add Trainer Button

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const selected = []
 
 let workSelectors = document.getElementsByClassName("workers");
 let dateSelector = document.getElementById("date");
+let trainerSelector = document.getElementsByClassName("trainerButton");
 
 let date = new Date();
 let today = date.getFullYear().toString() + '-' + (date.getMonth() + 1).toString().padStart(2, 0) +
@@ -18,4 +19,21 @@ for (const element of workSelectors) {
         
         element.appendChild(option)
     }); 
+}
+
+function addTrainer (e) {
+    const newElement = document.createElement('select');
+
+    newElement.name = "worker";
+    newElement.class = "workers"
+
+    team.forEach(member => {
+        console.log(member);
+
+        let option = new Option(member, member);
+        
+        newElement.appendChild(option)
+    });
+
+    e.outerHTML = newElement.outerHTML;
 }
